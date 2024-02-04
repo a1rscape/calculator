195a77154fe4da07007ae1bcaa490a6292fb37bf
Scape <hamlet.mihranyan@gmail.com>
2024-02-04 06:13:13 +0400

    final

---
+++ src/components/store.js
@@ -0,0 +1,1 @@
+let store = { keywords: [ { val: "7" }, { val: "8" }, { val: "9" }, { val: "4" }, { val: "5" }, { val: "6" }, { val: "1" }, { val: "2" }, { val: "3" }, ], operations: [ { val: "/" }, { val: "*" }, { val: "-" }, { val: "+" }, { val: "=" }, ], operationsTop: [ { val: "AC" }, { val: "C" }, { val: "%" }, ] }; export default store;
